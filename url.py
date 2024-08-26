f88773d99713c37de11c02e62b162ca5aa94a50b
Ibrahim Abdus-Sabur <ibby.abdussabur@gmail.com>
2024-08-26 10:32:20 -0400

    fix content-length issue by reading in binary mode

--- url.py
+++ url.py
@@ -56,25 +56,25 @@ class URL:
         # encode request as bytes to send
         self.socket.send(request.encode("utf8"))
         # read all responses into var
-        response = self.socket.makefile("r", encoding="utf8", newline="\r\n")
+        raw_response = self.socket.makefile("rb", encoding="utf8", newline="\r\n")
 
-        statusline = response.readline()
+        statusline = raw_response.readline().decode(encoding="utf-8")
         version, status, explanation = statusline.split(" ", 2)
 
         # grab headers
         response_headers = {}
-        line = response.readline()
+        line = raw_response.readline().decode(encoding="utf-8")
         while line != "\r\n":
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
-            line = response.readline()
+            line = raw_response.readline().decode(encoding="utf-8")
 
         assert status.isnumeric()
         status = int(status)
     
         # handle redirects in 300 range
         if status > 299 and status < 400 and 'location' in response_headers and redirect < REDIRECT_LIMIT:
-            response.close()
+            raw_response.close()
             location = response_headers['location']
             redirect_url = URL(location)
             print(f"redirect {redirect} to {location}")
@@ -84,7 +84,7 @@ class URL:
             else:
                 return redirect_url.make_http_request(redirect = redirect + 1)
         elif status > 299 and status < 400 and redirect >= REDIRECT_LIMIT:
-            response.close()
+            raw_response.close()
             location = response_headers['location']
             return (f"Redirect loop detected! Last redirect is to :{location}", 0)
 
@@ -95,10 +95,10 @@ class URL:
         # respect content-length
         if "content-length" in response_headers:
             content_length = int(response_headers["content-length"])
-            # TODO investigate why this doesn't work for large values 
-            content = response.read(content_length) 
+            content = raw_response.read(content_length).decode("utf-8") 
         else:
-            content = response.read()
+            content = raw_response.read().decode("utf-8")
+        raw_response.close()
 
         # get time content should be cached
         cache_time = 0
@@ -110,7 +110,6 @@ class URL:
                 elif directive.casefold() == 'nostore':
                     cache_time = 0
                     break
-        response.close()
         return content, cache_time
 
     def make_file_request(self):
