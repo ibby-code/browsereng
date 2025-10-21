be06c0f4ad83d28a7d11b0a3c86a237ea50c567f
Ibrahim Abdus-Sabur <ibby.abdussabur@gmail.com>
2025-10-21 12:28:00 -0400

    Record threading info in measurements

--- browser/measure_time.py
+++ browser/measure_time.py
@@ -1,9 +1,11 @@
+import threading
 import time
 import json
 
 
 class MeasureTime:
     def __init__(self):
+        self.lock = threading.Lock()
         self.file = open("browser_trace.json", "w")
         self.file.write('{"traceEvents": [')
         self.file.write(create_time_entry("process_name", "M",
@@ -11,18 +13,27 @@ class MeasureTime:
         self.file.flush()
 
     def time(self, name):
-        new_entry = create_time_entry(name, "B", {"tid": 1})
-        self.file.write(f", {new_entry}")
-        self.file.flush()
+        with self.lock:
+            tid = threading.get_ident()
+            new_entry = create_time_entry(name, "B", {"tid": tid})
+            self.file.write(f", {new_entry}")
+            self.file.flush()
 
     def stop(self, name):
-        new_entry = create_time_entry(name, "E", {"tid": 1})
-        self.file.write(f", {new_entry}")
-        self.file.flush()
+        with self.lock:
+            tid = threading.get_ident()
+            new_entry = create_time_entry(name, "E", {"tid": tid})
+            self.file.write(f", {new_entry}")
+            self.file.flush()
     
     def finish(self):
-        self.file.write("]}")
-        self.file.close()
+        with self.lock:
+            for thread in threading.enumerate():
+                new_entry = create_time_entry("thread_name", "M",
+                    {"tid": thread.ident, "args": {"name": thread.name}})
+                self.file.write(f", {new_entry}")
+            self.file.write("]}")
+            self.file.close()
 
 
 def create_time_entry(name: str, phase: str, metadata: dict) -> str:
